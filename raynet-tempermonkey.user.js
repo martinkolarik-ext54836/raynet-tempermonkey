7e29a73af820d1f931fe4d93d7a6e19e900baf47
Martin <2612765+martinkolarik-ext54836@users.noreply.github.com>
2025-10-21 14:07:35 +0200

    Bump version from 3.6a to 3.7

--- raynet-tempermonkey.user.js
+++ raynet-tempermonkey.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Raynet grid reformatter (visible-grid scoped)
 // @namespace    https://tampermonkey.net/
-// @version      3.6a
+// @version      3.7
 // @description  Attach toggle after every exact "Exportovať". Always rescan and apply ONLY to the currently visible grid/tab using a unique CSS scope. When enabled, clone the visible grid into a fullscreen popup and apply CSS to the clone.
 // @match        *://*.app.raynetcrm.sk/intertec*
 // @updateURL    https://raw.githubusercontent.com/martinkolarik-ext54836/raynet-tempermonkey/main/raynet-tempermonkey.user.js
